839179a4ad21a5e6b41c013e4fe2638c69b50d0d
Matt Jones <glitchyghost81@protonmail.com>
2022-12-12 10:30:14 +0000

    fixed navbar bug not sticking, improved spyscroll

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -5,20 +5,26 @@
 import "../../node_modules/popper.js/dist/popper.min.js";
 import "../../node_modules/bootstrap/js/dist/util.js";
 import "../../node_modules/bootstrap/js/dist/modal.js";
-const bootstrap = require('bootstrap')
+// const bootstrap = require('bootstrap')
 
 // hide toggle ?
 
-// const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
 
 // stop navlink animations repeating on hover
 const startPage = (() => {
     const html = document.documentElement,
-          s = 'start'
+        s = 'start'
     html.classList.add(s)
-    window.addEventListener('load', function() {
-      setTimeout(() => {
-        html.classList.remove(s)
-      }, 200)
+    window.addEventListener('load', function () {
+        setTimeout(() => {
+            html.classList.remove(s)
+        }, 350)
     })
-  })()
+})()
+
+// set an offset for spyscroll (cater for sticky navbar)
+
+$('#navbar').scrollspy({
+    offset: 200
+});
