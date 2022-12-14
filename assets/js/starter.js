f2d15e9632ab5a144f4564c38954cc28d2ac241e
Matt Jones <glitchyghost81@protonmail.com>
2022-12-14 12:19:13 +0000

    js modules flag

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,16 +1,3 @@
-// Importing JavaScript Bootstrap
-
-// Import just what we need
-// Popper.js util and modal dependencies
-import "../../node_modules/popper.js/dist/popper.min.js";
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
-// const bootstrap = require('bootstrap')
-
-// hide toggle ?
-
-const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
-
 // stop navlink animations repeating on hover
 const startPage = (() => {
     const html = document.documentElement,
@@ -23,8 +10,3 @@ const startPage = (() => {
     })
 })()
 
-// set an offset for spyscroll (cater for sticky navbar)
-
-$('#navbar').scrollspy({
-    offset: 200
-});
